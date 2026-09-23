08af259820081bf458e0e6955d57418fbe499d3f
АбдраковаМария <m.abdrakova@yandex.ru>
2026-09-23 23:13:57 +0300

    Сделана п1.3.6

--- 134-led-module/main.c
+++ 134-led-module/main.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 #include "led.h"
+#include "log.h"
 
 const uint BUTTON_PIN = 15;
 
@@ -18,16 +18,20 @@ void handle_command(int command)
     if (command == 'e')
     {
         led_set(true);
-        printf("led %s\n", led_is_on() ? "on" : "off");
+        LOG_INF("led %s\n", led_is_on() ? "on" : "off");
     }
     else if (command == 'd')
     {
         led_set(false);
-        printf("led %s\n", led_is_on() ? "on" : "off");
+        LOG_INF("led %s\n", led_is_on() ? "on" : "off");
+    }
+    else if (command == 'v')
+    {
+        log_version();
     }
     else
     {
-        printf("unknown command: %c\n", command);
+        LOG_ERR("unknown command: %c\n", command);
     }
 }
 
@@ -50,7 +54,7 @@ int main()
        if (previous == true && current == false)
        {
            led_toggle();
-           printf("led %s\n", led_is_on() ? "on" : "off");
+           LOG_INF("led %s\n", led_is_on() ? "on" : "off");
        }
 
        previous = current;
@@ -62,6 +66,7 @@ int main()
            continue;
        }
 
+       LOG_DBG("got %c\n", command);
        handle_command(command);
    }
 }
